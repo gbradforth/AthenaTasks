a1a9b9fe7d4c69b55c1b8804f66235295056849c
Sarah Chen <scart11235@gmail.com>
2023-02-25 23:24:42 -0800

    merged changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,41 @@
 import './App.css';
+import styled from 'styled-components';
+import { GoogleLogin } from '@react-oauth/google';
+import React, {useState} from 'react';
+import Calendar from './Calendar';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+const Button = styled.button`
+  display:inline-block;
+  flex: 1;
+  border: none;
+  background-color: #854C70;
+  color: white;
+  height: 30px;
+  width: 50px;
+  border-radius: 2px;
+  cursor: pointer;
+`;
+const Text = styled.input`
+  border: 2px solid #000;
+  width: 200px;
+  padding: 5px;
+  border-radius: 2px;
+  margin: 5px;
+`;
+const TaskCount = styled.span`
+  margin: 10px;
+`;
+const Tasks = styled.div`
+`;
+const LIST = styled.li`
+    listStyle:"none";
+    text-decoration: "line-through";
+`;
 
 function App() {
   const [input, setInput] = useState("");
@@ -56,35 +93,6 @@ function App() {
         <div class="row">
         <div id = "tasks">
                 <h2>Task List</h2> 
-                <Tasks>
-                  <TaskCount>
-                    <b>Pending Tasks</b> {todoList.length - completedTaskCount}
-                  </TaskCount>
-                  <TaskCount>
-                    <b>Completed Tasks</b> {completedTaskCount}
-                  </TaskCount>
-                </Tasks>
-              <div>
-                <ul>
-                  {todoList.map((todo) => {
-                    return (
-                      <LIST
-                        complete = {todo.complete}
-                        id={todo.id}
-                        onClick={() => handleComplete(todo.id)}
-                        style={{
-                          listStyle: "none",
-                          textDecoration: todo.complete && "line-through",
-                        }}
-                      >
-                        {todo.task}
-                      </LIST>
-                    );
-                  })}
-                </ul>
-              </div>
-              <Text value={input} onInput={(e) =>setInput(e.target.value)} />
-              <Button onClick={() => handleClick()}>Add</Button>
                 <Tasks>
                   <TaskCount>
                     <b>Pending Tasks</b> {todoList.length - completedTaskCount}
