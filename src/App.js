79313b0759509486f0309afe00fc150a12aed15e
Gwen Bradforth <gbradfor@usc.edu>
2023-02-25 16:57:03 -0800

    gwen App replace

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <body>
+        <h1><a href='index.html'>AthenaTasks</a></h1>
+        <div class="row">
+            <div id = "tasks">
+                <h2>Task List</h2> 
+                <input type="checkbox"/>
+                <label for="vehicle1"> Clean room</label>
+            </div>
+
+            <div id = "calendar">
+                <h2>Calendar</h2>
+            </div>
+
+            <div id="character">
+                <h2>Character</h2>
+            </div>
+        </div>
+      </body>     
   );
 }
 
